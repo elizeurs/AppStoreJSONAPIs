a22d6058c1bb6d96756fd9eaf263e116d137882f
elizeurs <13ders@gmail.com>
2021-07-29 16:08:21 -0300

    Handling User Selection

---
+++ AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  AppDetailController.swift
+//  AppStoreJSONApis
+//
+//  Created by Elizeu RS on 29/07/21.
+//
+
+import UIKit
+
+class AppDetailController: BaseListController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    collectionView.backgroundColor = .yellow
+  }
+}
