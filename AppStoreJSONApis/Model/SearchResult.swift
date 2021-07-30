d58c8a173edda475589b41d77d40543c065f2d52
elizeurs <13ders@gmail.com>
2021-07-30 01:56:17 -0300

    App Detail Info Cell

--- AppStoreJSONApis/Model/SearchResult.swift
+++ AppStoreJSONApis/Model/SearchResult.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 struct SearchResult: Decodable {
-let resultCount: Int
-let results: [Result]
+  let resultCount: Int
+  let results: [Result]
 }
 
 struct Result: Decodable {
-let trackName: String
-let primaryGenreName: String
-var averageUserRating: Float?
-let screenshotUrls: [String]
-let artworkUrl100: String //  app icon
+  let trackName: String
+  let primaryGenreName: String
+  var averageUserRating: Float?
+  let screenshotUrls: [String]
+  let artworkUrl100: String //  app icon
+  let formattedPrice: String
+  let description: String
+  let releaseNotes: String
 }
