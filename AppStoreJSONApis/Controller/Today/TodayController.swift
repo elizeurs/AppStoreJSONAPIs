547249cd2b801148d8589e02d0b01ed8c83945a3
elizeurs <13ders@gmail.com>
2021-08-04 19:27:15 -0300

    Cell abs Coordinates and Fullscreen Animation

--- AppStoreJSONApis/Controller/Today/TodayController.swift
+++ AppStoreJSONApis/Controller/Today/TodayController.swift
@@ -22,7 +22,40 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
   }
   
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    print("Animate fullscreen somehow...")
+    
+    let redView = UIView()
+    redView.backgroundColor = .red
+    redView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleRemoveRedView)))
+    view.addSubview(redView)
+//    redView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+    
+    guard let cell = collectionView.cellForItem(at: indexPath) else { return }
+    
+    // absolute coordinates of cell
+    guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
+    
+    self.startingFrame = startingFrame
+    redView.frame = startingFrame
+    redView.layer.cornerRadius = 16
+    
+    UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+      redView.frame = self.view.frame
+    }, completion: nil)
+    
+//    print("Animate fullscreen somehow...")
+  }
+  
+  var startingFrame: CGRect?
+  
+  @objc func handleRemoveRedView(gesture: UITapGestureRecognizer) {
+//    gesture.view?.removeFromSuperview()
+    // access startingFrame
+    UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
+//      redView.frame = self.view.frame
+      gesture.view?.frame = self.startingFrame ?? .zero
+    }, completion: { _ in
+      gesture.view?.removeFromSuperview()
+    })
   }
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
