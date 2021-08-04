fee9f096199b5d7c4604f8cd994677d1f44dd698
elizeurs <13ders@gmail.com>
2021-08-04 16:37:57 -0300

    Today Controller Cells

--- AppStoreJSONApis/Controller/BaseTabBarController.swift
+++ AppStoreJSONApis/Controller/BaseTabBarController.swift
@@ -40,10 +40,10 @@ class BaseTabBarController: UITabBarController {
 //      todayNavController,
 //      redNavController,
 //      blueNavController
+      createNavController(viewController: TodayController(), title: "Today", imageName: "today_icon"),
       createNavController(viewController:
                             AppsPageController(), title: "Apps", imageName: "apps"),
-      createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search"),
-      createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon")
+      createNavController(viewController: AppsSearchController(), title: "Search", imageName: "search")
     ]
   }
   
