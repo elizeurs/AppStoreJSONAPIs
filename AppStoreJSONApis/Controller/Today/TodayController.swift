cf20b61bef04e0a59cf4464cbe091410a6755831
elizeurs <13ders@gmail.com>
2021-08-06 16:34:58 -0300

    Auto Layout Constraint Animations

--- AppStoreJSONApis/Controller/Today/TodayController.swift
+++ AppStoreJSONApis/Controller/Today/TodayController.swift
@@ -21,7 +21,12 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     collectionView.register(TodayCell.self, forCellWithReuseIdentifier: cellId)
   }
   
-  var appFullscreenController: UIViewController!
+  var appFullscreenController: AppFullscreenController!
+  
+  var topConstraint: NSLayoutConstraint?
+  var leadingConstraint: NSLayoutConstraint?
+  var widthConstraint: NSLayoutConstraint?
+  var heightConstraint: NSLayoutConstraint?
   
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
@@ -40,19 +45,37 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else { return }
     
     self.startingFrame = startingFrame
-    redView.frame = startingFrame
+//    redView.frame = startingFrame
+    
+    
+    // auto layout constraint animations
+    // 4 anchors
+    redView.translatesAutoresizingMaskIntoConstraints = false
+    topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+    leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+    widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+    heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+    
+    [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
+    self.view.layoutIfNeeded()
+    
     redView.layer.cornerRadius = 16
     
     UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
       
-      redView.frame = self.view.frame
+      self.topConstraint?.constant = 0
+      self.leadingConstraint?.constant = 0
+      self.widthConstraint?.constant = self.view.frame.width
+      self.heightConstraint?.constant = self.view.frame.height
+      
+      self.view.layoutIfNeeded() // starts animation
+
+//      redView.frame = self.view.frame
       
       // hide tabbar is not working for some reason
 //      self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 100)
       
     }, completion: nil)
-    
-//    print("Animate fullscreen somehow...")
   }
   
   var startingFrame: CGRect?
@@ -62,7 +85,18 @@ class TodayController: BaseListController, UICollectionViewDelegateFlowLayout {
     UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut, animations: {
 //      redView.frame = self.view.frame
       
-      gesture.view?.frame = self.startingFrame ?? .zero
+      self.appFullscreenController.tableView.contentOffset = .zero
+      
+      // this frame code is bad
+//      gesture.view?.frame = self.startingFrame ?? .zero
+      guard let startingFrame = self.startingFrame else { return }
+      
+      self.topConstraint?.constant = startingFrame.origin.y
+      self.leadingConstraint?.constant = startingFrame.origin.x
+      self.widthConstraint?.constant = startingFrame.width
+      self.heightConstraint?.constant = startingFrame.height
+      
+      self.view.layoutIfNeeded()
       
       // bring back tabbar is not working for some reason
 //      self.tabBarController?.tabBar.transform = .identity
