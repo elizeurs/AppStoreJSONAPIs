3214a4f15f6067d327011797e588ab68427182a3
elizeurs <13ders@gmail.com>
2021-08-16 20:38:26 -0300

    Pagination User Interface

--- AppStoreJSONApis/Controller/BaseTabBarController.swift
+++ AppStoreJSONApis/Controller/BaseTabBarController.swift
@@ -12,34 +12,8 @@ class BaseTabBarController: UITabBarController {
   override func  viewDidLoad() {
     super.viewDidLoad()
     
-//    let todayViewController = UIViewController()
-//    todayViewController.view.backgroundColor = .white
-//    todayViewController.navigationItem.title = "Today"
-//    let todayNavController = UINavigationController(rootViewController: todayViewController)
-//    todayNavController.tabBarItem.title = "Today"
-//    todayNavController.tabBarItem.image = UIImage(named: "today_icon")
-//    todayNavController.navigationBar.prefersLargeTitles = true
-    
-//    let redViewController = UIViewController()
-//    redViewController.view.backgroundColor = .white
-//    redViewController.navigationItem.title = "Apps"
-//    let redNavController = UINavigationController(rootViewController: redViewController)
-//    redNavController.tabBarItem.title = "Apps"
-//    redNavController.tabBarItem.image = #imageLiteral(resourceName: "apps")
-//    redNavController.navigationBar.prefersLargeTitles = true
-    
-//    let blueViewController = UIViewController()
-//    blueViewController.view.backgroundColor = .white
-//    blueViewController.navigationItem.title = "Search"
-//    let blueNavController = UINavigationController(rootViewController: blueViewController)
-//    blueNavController.tabBarItem.title = "Search"
-//    blueNavController.tabBarItem.image = #imageLiteral(resourceName: "search")
-//    blueNavController.navigationBar.prefersLargeTitles = true
-    
     viewControllers = [
-//      todayNavController,
-//      redNavController,
-//      blueNavController
+      createNavController(viewController: MusicController(), title: "Music", imageName: "music"),
       createNavController(viewController: TodayController(), title: "Today", imageName: "today_icon"),
       createNavController(viewController:
                             AppsPageController(), title: "Apps", imageName: "apps"),
